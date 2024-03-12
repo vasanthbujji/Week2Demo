0d6a1018107283d6dd4d889542a96181345ea004
Vasanth Kumar Thota - STUDENT <l00186073@atu.ie>
2024-03-12 15:10:26 +0000

    commit

--- Week-2.R
+++ Week-2.R
@@ -31,3 +31,42 @@ sorted_z1<-sort(z, decreasing = FALSE)
 sorted_z2<-sort(z, decreasing = TRUE)
 sorted_z1
 sorted_z2
+
+
+1:30
+2*1:15
+30:1
+seq(15)
+seq_1<-seq(1:30)
+seq_1
+s3<-seq(-5,5,by=.2)
+s3
+seq3<-seq(-100,100,by=.6)
+seq3
+mean(seq3)
+sort(seq3, decreasing = FALSE)
+sort(s3, decreasing = FALSE)
+s4<-seq(length=51,from=-5,by=.2)
+s4
+
+
+#along with
+a<-c(1,4,6,9)
+b<-seq(from=0, to=20,along.with=a)
+a
+b
+
+
+seq(1,20)
+seq(20,1)
+seq(1,20,by=2)
+seq(1,20,length.out=30)
+2*seq(1,20)
+
+
+x1<-c(10.4,5.6,3.1,6.4,21.7)
+s5<-rep(x,times=5)
+s5
+s6<-rep(x,each=5)
+s6
+help("rep")
